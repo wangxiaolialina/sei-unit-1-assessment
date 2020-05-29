8df7f489d7b1be023ff1494c58d7ad4a8b8fb4c6
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-05-29 14:27:15 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+/*----- constants -----*/
+const INITIALPOINT = 0;
+const INITIALINPUT = 1;
+/*----- app's state (variables) -----*/
+let pointNum = INITIALPOINT;
+let inputNum = INITIALINPUT;
+/*----- cached element references -----*/
+let pointEl = document.querySelector('.points');
+let inputEl = document.querySelector('.input');
+
+/*----- event listeners -----*/
+/*----- functions -----*/
+
+
+
